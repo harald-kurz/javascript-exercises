ea2e2828ac3cb812450d00d60c3e08969d4b1c5f
harald-kurz <46395850+harald-kurz@users.noreply.github.com>
2024-10-26 16:59:41 +0200

    03 complete

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function (text) {
 
+  let arr = new Array();
+
+  for (let i = 0; i < text.length; i++) {
+
+    arr.unshift(text[i]);
+
+  }
+
+  return arr.join('');
 };
 
 // Do not edit below this line
