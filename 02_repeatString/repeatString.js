a63643a04d81c030df78f7b527a425d4bba8a9b1
harald-kurz <46395850+harald-kurz@users.noreply.github.com>
2024-10-26 16:56:31 +0200

    02 complete

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (text, num) {
+  if (num >= 0) {
+    let response = new Array();
 
+    for (let i = 0; i < num; i++) {
+      response.push(text);
+    }
+
+    return response.join('');
+  }
+  else {
+    return 'ERROR';
+  }
 };
 
 // Do not edit below this line
