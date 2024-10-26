05ebc0322f1c733f7f3c1e430855dbe3154c7734
harald-kurz <46395850+harald-kurz@users.noreply.github.com>
2024-10-26 17:06:51 +0200

    04 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+
+  let newArr = new Array();
+
+  arr.forEach(element => {
+    if (!args.includes(element)) {
+      newArr.push(element);
+    }
+  });
+
+  return newArr;
 };
 
 // Do not edit below this line
