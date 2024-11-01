52866b7cec9496f2d9516fd1d2163d0000d9f402
Harald Kurz <46395850+harald-kurz@users.noreply.github.com>
2024-11-01 15:55:32 +0100

    07 complete

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  const result = ((fahrenheit - 32) * (5/9)).toFixed(1);
+  return parseFloat(result);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  const result = ((celsius * (9/5)) + 32).toFixed(1);
+  return parseFloat(result);
 };
 
 // Do not edit below this line
