934d8d2c014724fac406b42670d60d6a2a8faa55
Harald Kurz <46395850+harald-kurz@users.noreply.github.com>
2024-11-01 17:20:12 +0100

    10 complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  let vorige;
+  let aktuelle;
 
+  num = parseInt(num);
+
+  if (num < 0) {
+    aktuelle = 'OOPS';
+  } else {
+    
+    for (let index = 0; index < num; index++) {
+      if (index === 0) {
+        vorige = 0;
+        aktuelle = 1;
+      } else {
+        const tmp = aktuelle;
+        aktuelle += vorige;
+        vorige = tmp;
+      }
+    }
+}
+
+  return num === 0 ? 0 : aktuelle;
 };
 
 // Do not edit below this line
