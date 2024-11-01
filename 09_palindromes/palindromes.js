743e23b4b439ad167405991fc865902323a8a8fe
Harald Kurz <46395850+harald-kurz@users.noreply.github.com>
2024-11-01 16:32:48 +0100

    09 complete

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (text) {
+  let textWithoutWhitespace = text.replace(/\s/g, '');
+  
+  var punctuation = /[.,\/#!$%\^&\*;:{}=\-_`~()]/g;
+  var textWithoutPunctiation = textWithoutWhitespace.replace(punctuation, '');
 
+  let compareArr = new Array();
+
+  for (let index = 0; index < textWithoutPunctiation.length; index++) {
+    compareArr.unshift(textWithoutPunctiation[index]);
+  }
+
+  const compareText = compareArr.join('');
+
+  return textWithoutPunctiation.toLowerCase() === compareText.toLowerCase();
 };
 
 // Do not edit below this line
