7d44b97f29432ab1dd8473bcf99f156520ae6202
Harald Kurz <46395850+harald-kurz@users.noreply.github.com>
2024-11-01 13:30:47 +0100

    05 complete

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+  if ( (Number.isInteger(num1) && Number.isInteger(num2)) && num1 >= 0 && num2 >= 0 )
+  {
+    if (num1 > num2){
+      const placeholder = num2;
+      num2 = num1;
+      num1 = placeholder;
+    }
+
+    let result = 0;
+
+    for (let index = num1; index <= num2; index++) {
+      result += index;
+    }
+
+    return result;
+  }
+  else{
+  return 'ERROR';
+  }
 };
 
 // Do not edit below this line
