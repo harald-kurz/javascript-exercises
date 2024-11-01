f6fdea1362d681a652f4106c7bd19e88e5a034d7
Harald Kurz <46395850+harald-kurz@users.noreply.github.com>
2024-11-01 16:03:43 +0100

    08 complete

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,40 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let result = 0;
+
+  for (const element of arr) {
+    result += element;
+  }
+
+  return result;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let result = 1;
 
+  for (const element of arr) {
+    result *= element;
+  }
+
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+	return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0 || n === 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
