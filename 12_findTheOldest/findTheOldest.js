62bf334d94efcee778a8278fed06f1f494c989d3
Harald Kurz <46395850+harald-kurz@users.noreply.github.com>
2024-11-01 17:43:09 +0100

    12 complete

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+  let oldestPerson;
+  let oldestAge = 0;
 
+  for (const element of arr) {
+    let age = ((element.yearOfDeath === undefined ? new Date().getFullYear() : element.yearOfDeath) - element.yearOfBirth);
+
+    if (age > oldestAge) {
+      oldestPerson = element;
+      oldestAge = age;
+    }
+  }
+
+  return oldestPerson;
 };
 
 // Do not edit below this line
