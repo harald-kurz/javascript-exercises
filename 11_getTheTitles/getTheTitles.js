a91b359ea4a514b1a961caa043ac25aed3058557
Harald Kurz <46395850+harald-kurz@users.noreply.github.com>
2024-11-01 17:25:16 +0100

    11 complete

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+  let result = new Array();
+  
+  for (const element of arr) {
+    result.push(element.title);
+  }
 
+  return result;
 };
 
 // Do not edit below this line
